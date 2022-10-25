d6c740c701a802aa2e799f8a6d722d55beef5b4b
Atul kharwal <mipl@abcs-MacBook-Pro.local>
2022-10-25 12:37:35 +0530

    first commit

---
+++ components/PrimaryButton.jsx
@@ -0,0 +1,11 @@
+/** @format */
+
+import { View, Text } from "react-native";
+
+export const PrimaryButton = ({ children }) => {
+  retun(
+    <View>
+      <Text>{children}</Text>
+    </View>
+  );
+};
---
+++ screens/StartGameScreen.jsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import { TextInput, View } from "react-native";
+import PrimaryButton from '../components/PrimaryButton'
+
+export const StartGameScreen = () => {
+  return (
+    <View>
+      <TextInput />
+      <PrimaryButton>Click me</PrimaryButton>
+    </View>
+  );
+};
